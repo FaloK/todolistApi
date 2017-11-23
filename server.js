910a09044fdbd2b9f2bf143e2ba47c4da23857cd
FaloK <falo@kru.yt>
2017-11-23 17:58:09 +0100

    New changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('localhost/Tododb');
+mongoose.connect('mongodb://localhost/Tododb');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
